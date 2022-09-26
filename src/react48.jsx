b7b8a28d2024ca3404a307bf0dd210f8abe2338c
WhitRavn <jmr4277@orange.fr>
2022-09-26 18:05:03 +0200

    content

---
+++ src/react48.jsx
@@ -0,0 +1,36 @@
+import { render } from "react-dom";
+import React, {
+  useState,
+  useMemo,
+  useCallback,
+  useContext,
+  createContext,
+} from "react";
+import PropTypes from "prop-types";
+
+function addition(n, a) {
+  return n + a;
+}
+
+Modal.propTypes = {
+    onClose: PropTypes.func.isRequired
+    children: PropTypes.node.isRequired
+}
+
+function Double({ n }) {
+  return "Le double de " + { n } + " est " + { n };
+}
+
+Double.propTypes = {
+  n: PropTypes.number.isRequired,
+};
+
+function App() {
+  return (
+    <div>
+      <Double n={1} />
+    </div>
+  );
+}
+
+render(<App />, document.querySelector(".app"));
